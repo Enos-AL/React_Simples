724b025e9815277c659b9a1d8adf3c328ac99f54
Enos-AL <ciscopackettracent@gmail.com>
2024-09-06 10:54:19 -0300

    Correção e organização de pastas

--- backend/app.ts
+++ backend/app.ts
@@ -1,17 +1,27 @@
 import express from 'express';
 import cors from 'cors';
-import path from 'path';
-import routes from './routes';
+import path from 'path'; // Importando 'path' para manipular caminhos de diretórios
+import routes from './src/routes/usuarioRoutes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Rotas
+// Rotas da API
 app.use('/api', routes);
+
+// Usando o 'path' para servir arquivos estáticos (por exemplo, para servir o frontend depois de construído)
+const staticPath = path.join(__dirname, '../frontend/build');
+app.use(express.static(staticPath));
+
+// Servir o frontend em produção
+app.get('*', (req, res) => {
+  res.sendFile(path.join(staticPath, 'index.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
